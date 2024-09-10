a03f10a2a6df2be4c46abf3bf531f9b986992f1b
RC_Student_lab <RC_Student_lab@10.0.0.29>
2024-09-10 11:35:08 +0000

    firstcommit

---
+++ src/main/java/com/mycompany/stringmethods/Stringmethods.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stringmethods;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Stringmethods {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+   //charAt()";
+
+   String name = "Hello";
+   String lname = "Hello";
+    char results = name.charAt(0);
+    System.out.println(results);
+    System.out.println(name.charAt(3));
+    
+    
+    
+    //compareTo()
+    System.out.println(name.compareTo(lname));
+    
+    
+    
+    //concat() Returns a string 
+    
+    System.out.println(name.concat(name));
+    
+    
+    }  
+} 
